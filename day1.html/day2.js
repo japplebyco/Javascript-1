8ad04d2b25e7b00683c520a7eaa966f2ad263d64
japplebyco <japplebyco@gmail.com>
2016-02-24 20:49:50 -0800

    day2work

---
+++ day1.html/day2.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+var num1 = prompt("Pick a whole number");
+
+ console.log(num1);
+ 
+ var num2 = prompt("Pick another whole number");
+ 
+ console.log(num2);
+ 
+var sum = Number(num1) + Number(num2);
+ alert(num1+ " plus " + num2 + " is: " + sum);
+ console.log(sum);
+ 
+ 
