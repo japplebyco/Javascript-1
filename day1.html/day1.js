37d5639dec388ad63d07138930fcfc42446afdd0
japplebyco <japplebyco@gmail.com>
2016-02-24 19:06:11 -0800

    first commit js1

---
+++ day1.html/day1.js
@@ -0,0 +1,16 @@
+"use strict";
+//alert ("hello world");
+var favFood = "cheese";
+var favDrink = "weizenbier";
+var favFoodAndDrink = favFood + ", " + favDrink;
+console.log (favFoodAndDrink);
+var num1=1;
+var num2=2;
+var sum=num1+num2;
+var average=sum/2 ;
+console.log (sum);
+console.log(average);
+
+var numArray=[1,2];
+sum =numArray[0]+numArray[1];
+console.log(sum);
