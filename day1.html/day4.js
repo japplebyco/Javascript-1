8cf5d2a580eebc0459d1782059972f70bcc99e25
japplebyco <japplebyco@gmail.com>
2016-03-02 19:40:05 -0800

    story

---
+++ day1.html/day4.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var num1 = prompt("Enter a number: ");
+
+console.log(num1);
+
+var num2 = prompt("Enter another number: ");
+
+console.log(num2);
+
+var result1 = parseInt(num1) * parseInt(num2);
+
+alert(result1);
+
+console.log (result1);
+
+var result2 = parseInt(num1) / parseInt(num2);
+
+alert(result2);
+
+console.log (result2);
+
+
+
+
+
+ 
+ 
+ 
