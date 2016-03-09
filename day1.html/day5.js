312f4823dc66ce2b18397ee6434a4e9028e635bc
japplebyco <japplebyco@gmail.com>
2016-03-08 21:13:52 -0800

    day5hw

---
+++ day1.html/day5.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+function square(number) {
+	if (NaN || typeof true === 'boolean' || typeof false === 'boolean') {
+		return NaN;
+	}
+  return number * number;
+}
+
+function capitalize(str) {
+    if (!str || typeof str !== "string"){
+		return ("");
+	}
+	
+	return str[0].toUpperCase() + str.slice(1) + ".";
+}
+
+function reverse(str){
+	if (!str || typeof str !== "string" || str.length < 2){
+		return ("");
+	}
+	
+	
+    return str.split("").reverse().join("");
+}
+
+
+function calc(a,b,x,y){
+	if (!typeof [] || typeof str == "string" || typeof true === 'boolean' || typeof false === 'boolean' ){
+		return (NaN);
+	}type
+var average=((a+b+x+y)/4).toFixed(4)
+return(average)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
